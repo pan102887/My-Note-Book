94179a229b2cb25f7dcb48941f0921d1d470262a
pansf <pansf@xiaopeng.com>
2024-05-31 18:24:22 +0800

    remove useless

--- work/xp-lark/常用项目/coupon_package.sql
+++
@@ -1,5 +0,0 @@
-select count(1)/4 as total
-  from `coupon_package_record`
-  where create_time between '2022-10-01 00:00:00' and '2022-11-01 00:00:00'
-  group by `coupon_package_plan_id`
- order by `id` desc;
