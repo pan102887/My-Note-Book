d826f831f6396b96641468863580611345d90738
pansf <pansf@xiaopeng.com>
2025-10-31 18:32:45 +0800

    feat: Add Kafka partition expansion section and new MySQL documents
    - Added a section on Kafka partition expansion and its potential for message loss in the Kafka documentation.
    - Created new documents for MySQL, including InnoDB storage engine, MySQL sharding strategies, and common MySQL issues.
    - Introduced a Jupyter notebook for testing with markdown and code cells.
    - Documented the experience of a Pinduoduo interview and described a rate limiting component project.

--- tes.sh
+++
@@ -1,7 +0,0 @@
-
-curl -X POST http://localhost:8080/logan/store/queryNearestStore --header 'Content-Type: application/json' -d '{"lng":113.385002,"lat":23.1593}'
-
-cms:store:show_and_test_drive_cars:COS56701
-cms:store:show_and_test_drive_cars:C944AOS01
-
-echo -e "Auth "
